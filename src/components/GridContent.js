624ff66a0b3ec80b44ec4486cf30a57e1ddb2bad
tomiiide <ayotomiiide@gmail.com>
2017-03-10 08:19:12 +0100

    copy to clipboard works

--- src/components/GridContent.js
+++ src/components/GridContent.js
@@ -2,13 +2,45 @@ import React, { Component } from 'react';
 import { GridList, GridTile } from 'material-ui/GridList';
 import FlatButton from 'material-ui/FlatButton';
 
+
 class GridContent extends Component {
   constructor(props) {
     super(props);
     this.state = {
       codes : props.codes
     }
+     this.copyToClipboard = this.copyToClipboard.bind(this);  
   } 
+
+  copyToClipboard(text) {
+    var input = document.createElement("input");
+    input.type = "text";
+    
+    input.style.position = 'fixed';
+    input.style.top = 0;
+    input.style.left = 0;
+    input.style.width = '2em';
+    input.style.height = '2em';
+    input.style.padding = 0;
+    input.style.border = 'none';
+    input.style.outline = 'none';
+    input.style.boxShadow = 'none';
+    input.style.background = 'transparent';
+    
+    document.body.appendChild(input);
+    input.value = text;
+    input.select();
+
+    try {
+      var success = document.execCommand('copy');
+      var msg = success ? 'sucessful' : 'unsucessful'
+      console.log("Copying text command was ", msg)
+      input.parentNode.removeChild(input);
+    } catch (err) {
+      console.log("OOps, unable to copy");
+    }
+}
+
   render() {
     return (
             <GridList style={{padding:"10px"}}>
@@ -16,7 +48,8 @@ class GridContent extends Component {
               <GridTile
                 key={code.code}
                 title={code.details}
-                style={{textAlign:"center"}}
+                style={{ textAlign: "center" }}
+                onClick={() => {this.copyToClipboard(code.code)}}
               > <FlatButton style={{ fontSize: "45px", width: "100%", height: "100%" }}
                 labelStyle={{ height: "100%", width: "100%" }}>{code.code}</FlatButton>
               </GridTile>
